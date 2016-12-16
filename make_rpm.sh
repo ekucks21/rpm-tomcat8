da26f346bc3b7c8c5f6d63d6d6f13e389af44633
Eric Kucks <kuckse@pragmatics.com>
2016-12-16 09:12:08 -0500

    removed publish-rpm command

--- make_rpm.sh
+++ make_rpm.sh
@@ -21,6 +21,3 @@ ln -v -s "$(pwd)/tomcat8.spec" "rpmbuild/SPECS/"
 cd rpmbuild
 
 rpmbuild --buildroot "`pwd`/BUILDROOT" ../tomcat8.spec -bb --define "_topdir `pwd`"
-
-publish-rpm $VERSION $RELEASE $ARCH tomcat8 "RPMS/$ARCH/tomcat8-$VERSION-$RELEASE.$ARCH.rpm"
-
