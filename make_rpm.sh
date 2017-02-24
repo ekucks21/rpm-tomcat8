4feaba92142d31d3e84254c7fb9716da6005437b
Eric Kucks <kuckse@pragmatics.com>
2017-02-24 15:05:58 -0500

    Revert "JFW-8910 make_rpm.sh downloads non prag tomcat"
    This reverts commit ce4959aa9c35f28993d9429570fe083a5e5b7f93.

--- make_rpm.sh
+++ make_rpm.sh
@@ -1,7 +1,6 @@
 VERSION=$(grep "Version:" tomcat8.spec |cut -d ":" -f2 |tr -d "[:space:]")
 RELEASE=$(grep "Release:" tomcat8.spec |cut -d ":" -f2 |tr -d "[:space:]")
 ARCH=$(grep "BuildArch:" tomcat8.spec |cut -d ":" -f2 |tr -d "[:space:]")
-ORIGINAL_VERSION=$(echo "$VERSION" | sed s/.prag//)
 
 echo "Version: $VERSION-$RELEASE BuildArch: $ARCH"
 
@@ -13,9 +12,9 @@ mkdir rpmbuild/SOURCES
 mkdir rpmbuild/SPECS
 mkdir rpmbuild/SRPMS
 
-wget http://archive.apache.org/dist/tomcat/tomcat-8/v$ORIGINAL_VERSION/bin/apache-tomcat-$ORIGINAL_VERSION.tar.gz -O apache-tomcat-$ORIGINAL_VERSION.tar.gz
+wget http://archive.apache.org/dist/tomcat/tomcat-8/v$VERSION/bin/apache-tomcat-$VERSION.tar.gz -O apache-tomcat-$VERSION.tar.gz
 
-ln -v -s "$(pwd)/apache-tomcat-$ORIGINAL_VERSION.tar.gz" "rpmbuild/SOURCES/"
+ln -v -s "$(pwd)/apache-tomcat-$VERSION.tar.gz" "rpmbuild/SOURCES/"
 ln -v -s "$(pwd)/tomcat8."{init,logrotate,sysconfig,bin,conf} "rpmbuild/SOURCES/"
 ln -v -s "$(pwd)/tomcat8.spec" "rpmbuild/SPECS/"
 
